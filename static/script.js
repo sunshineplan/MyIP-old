242f1020cb9cbfde168ff1508b9031fe2570b76b
sunshineplan <sunshineplan@users.noreply.github.com>
2020-06-03 21:11:44 +0800

    Use jsdelivr and sweetalert2 instead of cdnjs and built-in alert

--- static/script.js
+++ static/script.js
@@ -2,9 +2,8 @@ function getInfo(search = false) {
     $('#localtime').text(new Date().toString().split('(')[0].trim());
     $('#platform').text(navigator.platform);
     $('#useragent').text(navigator.userAgent);
-    if (search) {
-        doQuery($('#input').val());
-    } else {
+    if (search) doQuery($('#input').val());
+    else {
         $('#input').val('');
         doQuery();
     };
@@ -12,27 +11,27 @@ function getInfo(search = false) {
 
 function doQuery(ip = '') {
     var api = 'https://api.ipdata.co/{ip}?api-key=' + API_KEY;
-    if ($('#online').prop('checked')) {
+    if ($('#online').prop('checked')) query('/query?ip=' + ip);
+    else if (ip == '' || ipaddr.isValid(ip)) query(api.replace('{ip}', ip));
+    else {
+        console.log('Host must be searched on server side.');
         query('/query?ip=' + ip);
-    } else {
-        if (ip == '' || ipaddr.isValid(ip)) {
-            query(api.replace('{ip}', ip));
-        } else {
-            console.log('Host must be searched on server side.');
-            query('/query?ip=' + ip);
-        };
     };
 };
 
 function query(api) {
+    var BootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'swal btn btn-primary'
+        },
+        buttonsStyling: false
+    });
     $('.info').text('');
     $('.info').addClass('loading');
     $('.flag').removeAttr('src');
     $.getJSON(api).done(json => {
         document.title = 'IP: ' + json.ip;
-        $.each(json, function (key, value) {
-            fill(key, value);
-        });
+        $.each(json, (key, value) => fill(key, value));
         fill('isp', json.asn, 'name');
         fill('org', json.carrier, 'name');
         fill('timezone', json.time_zone, 'name');
@@ -41,28 +40,17 @@ function query(api) {
         document.title = 'My IP';
         $('.info').text('N/A');
         try {
-            alert('Error!\n\n' + error.responseJSON.message);
+            BootstrapButtons.fire('Error', error.responseJSON.message, 'error');
         } catch (e) {
-            alert('Error!');
+            BootstrapButtons.fire('Error', 'Unknown error!', 'error');
         }
-    }).always(() => {
-        $('.info').removeClass('loading');
-    });
+    }).always(() => $('.info').removeClass('loading'));
 };
 
 function fill(element, data, key = '') {
+    var value;
     if (data != undefined) {
-        if (key == '') {
-            var value = data;
-        } else {
-            var value = data[key];
-        };
-        if (value != '' && value != null) {
-            $('#' + element).text(value);
-        } else {
-            $('#' + element).text('N/A');
-        };
-    } else {
-        $('#' + element).text('N/A');
-    };
+        if (key == '') value = data; else value = data[key];
+        if (value != '' && value != null) $('#' + element).text(value); else $('#' + element).text('N/A');
+    } else $('#' + element).text('N/A');
 };
